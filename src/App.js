d5bf2f560f4ff3c4d7c4677e7d189f1645fda4e5
ShamiTomita <shamitomita@gmail.com>
2022-07-27 14:19:00 -0400

    sidebare fixed in position and main contnet right

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Nav } from './nav';
 import { Main } from './components';
+import Sidebar from './components/Sidebar';
 import NavProvider from './context/NavContext';
 import './App.css';
 
 function App() {
 	return (
 		<div className='appContainer'>
+
 			<NavProvider>
 				<Nav />
+				<Sidebar/>
 				<Main />
 			</NavProvider>
+			
 		</div>
 	);
 }
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import './index.scss';
+import {Link, NavLink} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome, faUser, faEnvelope, faFolder} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+const Sidebar = () => (
+  <div className="nav-bar">
+    <ul>
+      <li>
+        <a
+        href="https://www.linkedin.com/in/shami-tomita-rodrgiuez/"
+        target="_blank"
+        rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#D4D4D4" />
+        </a>
+      </li>
+      <li>
+        <a
+         href="https://github.com/ShamiTomita"
+         target="_blank"
+         rel="noreferrer"
+         >
+          <FontAwesomeIcon icon={faGithub} color="#D4D4D4" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar;
