abdf621d86142ea943a1c2fa371550bae9f56814
ShamiTomita <shamitomita@gmail.com>
2022-07-27 12:26:53 -0400

    Nav, NavLinks, CSS

---
+++ src/nav/NavLink.js
@@ -0,0 +1,5 @@
+export const navLinks = [
+  { navLinkId: 'Home', scrollToId: 'homeContainer'},
+  { navLinkIdL: 'About', scrollToId: 'aboutContainer'},
+  { navLinkId: 'Contact', scrollToId: 'contactContainer'}
+];
