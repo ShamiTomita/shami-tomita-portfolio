9de52ddbed1d6c47eb9725458b4b842b54543c20
ShamiTomita <shamitomita@gmail.com>
2022-08-03 19:49:43 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -17,42 +17,42 @@ const About = () => {
 					<h3>ABOUT</h3>
 					<h4>Born in Mexico City, Raised in Texas, Educated in NYC</h4>
 					<p>*Graduated from NYU in 2017 with a Bachelor of Arts in Music and Game Design</p>
-					<p>*Worked as a Line Cook for Xian Famous Foods and an Assistant Pastry Chef @ Ps. & Co</p>
-					<p>*Completed the Flatiorn School's Software Engineering Bootcamp</p>
+					<p>*Line Cook for Xian Famous Foods</p>
+					<p>*Assistant Pastry Chef @ Pure Sweets & Co.</p>
+					<p>*Graduate of the Flatiron School's Full Stack Software Engineering Program</p>
 					<p></p>
 				</div>
-				<div className="about cakes">
-					<img
-						src='./cakes.jpg'
-						alt='unsplash-img'
-					/>
-				</div>
 			</div>
+
 			<div className="about-photo">
 				<div>
 					<div className="container">
+						<a href="https://cas.nyu.edu/" target="_blank" rel="noopener noreferrer">
+							<div className="overlay">
+							</div>
+						</a>
 						<img src="./nyu.png" alt="NYU Logo"/>
-						<div className="overlay">
-							<div className="text">NYU</div>
-						</div>
 					</div>
 					<div className="container">
 						<img src="./psco.png" alt="Pure Sweets and Company Logo" />
-						<div className="overlay">
-							<div className="text">Pure Sweets</div>
-						</div>
+						<a href="https://www.instagram.com/psandco/?hl=en" target="_blank" rel="noopener noreferrer">
+							<div className="overlay">
+							</div>
+						</a>
 					</div>
 						<div className="container">
 								<img src="./flatironschool.png" alt="The Flatiron School Logo" />
-								<div className="overlay">
-									<div className="text">Flatiron</div>
-								</div>
+								<a href="https://flatironschool.com/courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">
+									<div className="overlay">
+									</div>
+								</a>
 						</div>
 					<div className="container">
 						<img src="./XFF.png" alt="Xian Famous Foods Logo"/>
-						<div className="overlay">
-							<div className="text">XFF</div>
-						</div>
+						<a href="https://www.xianfoods.com/" target="_blank" rel="noopener noreferrer">
+							<div className="overlay">
+							</div>
+						</a>
 					</div>
 
 
