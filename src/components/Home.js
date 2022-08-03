5fa28e7fa58cf75302729a2c684ceea0c07a52df
ShamiTomita <shamitomita@gmail.com>
2022-08-03 13:28:24 -0400

    home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,72 @@
 import React from 'react'
 import { useNav } from '../customHooks/useNav';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import './Components.css';
 import Marquee from "react-fast-marquee"
 
 const Home = () => {
   const homeRef = useNav('Home');
+  library.add(fab)
   return(
-    <section ref={homeRef} id='homeContainer'>
+    <>
+      <section ref={homeRef} id='homeContainer'>
+        <div className="ticker">
+          <Marquee
+            gradient={false}
+            gradientColor={[162, 171, 106]}
+            direction="left"
+            speed={80}
+            className="marquee"
+          >
+            <h1 className="word">
+              🪴 Hola! 🌸
+            </h1>
+            <h1 className="word">
+              🍛 こんにちは! 🍰
+            </h1>
+            <h1 className="word">
+              🕹 Hello! 💻
+            </h1>
+          </Marquee>
+        </div>
+        <div className="home">
 
-      <div className="home">
-        <Marquee
-          gradient={false}
-          gradientColor={[162, 171, 106]}
-          direction="left"
-          speed={80}
-          className="ticker"
-        >
-          Hola! こんにちは! Hello! Hola! こんにちは! Hello! Hola! こんにちは! Hello!
-        </Marquee>
-        <img className="shami" src="./portfolio_pic.png" alt="picture of shami :)"/>
-        <h2>I'm Shami Tomita</h2>
-        <h3>Fullstack Dev | Pastry Chef | Music Lover | Cat Parent</h3>
-        <p>Welcome to my portfolio! Please Hire Me also I'm DESPERATE</p>
-      </div>
+          <div>
+            <img className="shami" src="./portfolio_pic.png" alt="shami :)"/>
+
+          </div>
+
+          <div className="homeblurb">
+            <h1>I'm Shami Tomita</h1>
+            <h2>Full Stack Dev | Pastry Chef | Cat Parent</h2>
+            <p>Welcome to my portfolio! Please Hire Me also I'm DESPERATE</p>
+
+          </div>
+        </div>
 
 
+        <div className="bottom">
+          <Marquee>
+            <FontAwesomeIcon className="icon" icon="fab fa-js" />
+            <img className="icon" src="./icons8-html-5.svg" alt=""/>
 
+            <img className="icon" src="./icons8-css3.svg" alt=""/>
 
-    </section>
+            <FontAwesomeIcon  className="icon" icon="fab fa-github" />
+            <FontAwesomeIcon  className="icon" icon="fab fa-react" />
+            <img className="icon" src="./icons8-redux.svg" alt=""/>
+            <FontAwesomeIcon className="icon" icon="fab-brands fa-html5"/>
+            <img className="icon" src="./icons8-ruby-programming-language.svg" alt=""/>
+            <img className="icon" src="./icons8-ruby-on-rails.svg" alt=""/>
+
+          </Marquee>
+
+        </div>
+
+      </section>
+    </>
   );
 };
 
