eaba70b1375ab49140bdb62b6549ee2e1e11bd55
ShamiTomita <shamitomita@gmail.com>
2022-08-03 19:50:09 -0400

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,13 +6,9 @@ const Home = () => {
   const contactRef = useNav('Home');
   return(
     <section ref={contactRef} id='contactContainer'>
-      <img
-        src='https://images.wallpaperscraft.com/image/single/moss_grass_tree_stump_wood_106893_1280x720.jpg'
-        alt='moss background'
-      />
       <div>
         <h3>Contact</h3>
-        <p>Welcome to my portfolio! Please Hire Me also I'm NOT DESPERATE</p>
+        <p></p>
       </div>
     </section>
   );
